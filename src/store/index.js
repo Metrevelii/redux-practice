eb33e0d4ea09ab2b8bf837283915fd2d3aa94d28
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-11-14 16:22:52 +0400

    adding state slices using createSlice method

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,57 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = { counter: 0, showCounter: true};
+
+createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
 
 // 3 Creating reducer function
-const counterReducer = (state = {counter: 0}, action) => {
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter,
         };
     }
     if (action.type === 'increase') {
         return {
             counter: state.counter + action.amount,
+            showCounter: state.showCounter,
         };
     }
     if (action.type === 'decrement') {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter,
         };
     }
+
+    if (action.type === 'toggle') {
+        return {
+            // inverting the value: if it was true - becomes false ...
+            showCounter: !state.showCounter,
+            counter: state.counter
+        };
+    }
+
     return state;
 }
 
