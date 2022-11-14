0a96edc8a57b8edc16a739c696dedd24720886fa
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-11-14 17:08:29 +0400

    Connecting Redux to toolkit state

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+// import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 
 const initialState = { counter: 0, showCounter: true};
 
-createSlice({
+const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
@@ -23,40 +23,43 @@ createSlice({
     }
 });
 
-// 3 Creating reducer function
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        };
-    }
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        };
-    }
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === 'toggle') {
-        return {
-            // inverting the value: if it was true - becomes false ...
-            showCounter: !state.showCounter,
-            counter: state.counter
-        };
-    }
-
-    return state;
-}
 
 //2 Creating store for redux
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
 
 export default store;
+
+// // 3 Creating reducer function
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+
+//     if (action.type === 'toggle') {
+//         return {
+//             // inverting the value: if it was true - becomes false ...
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         };
+//     }
+
+//     return state;
+// }
