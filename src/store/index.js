733d9d34f197caf3ac2cdb8b0862b90d4e109262
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-11-14 14:52:03 +0400

    added redux, added provider

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+// 3 Creating reducer function
+const counterReducer = (state = {counter: 0}, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1,
+        };
+    }
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1,
+        };
+    }
+    return state;
+}
+
+//2 Creating store for redux
+const store = createStore(counterReducer);
+
+
+export default store;
